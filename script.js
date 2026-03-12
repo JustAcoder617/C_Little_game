7bdd5dde1b12e7e9b0d2d2da5cf18d17736a455e
Alysson <antonioalvesdasilvaalysson0@Gmail.com>
2026-03-12 10:05:02 -0300

    completed site

---
+++ script.js
@@ -0,0 +1,10 @@
+const button=document.getElementById("repo");
+function onrepo(){
+    window.location.href='https://github.com/JustAcoder617/Whispers-of-Condemnation/blob/main/modularization/cweb.h'
+}
+button.addEventListener("click", onrepo);
+function onperson(){
+    window.location.href='https://github.com/JustAcoder617';
+}
+const person=document.getElementById("person");
+person.addEventListener("click", onperson);
